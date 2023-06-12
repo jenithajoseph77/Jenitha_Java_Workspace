3e53d51b3171dd409f2bc4a33be40cd242d33dcb
jenithajoseph77 <87360618+jenithajoseph77@users.noreply.github.com>
2023-06-12 15:10:47 +0530

    Add files via upload

---
+++ 06-06-2023/task2.sql
@@ -0,0 +1,11 @@
+--multiple column subquery
+select empno,ename,mgr,deptno from emp where (mgr,deptno) in
+(select mgr,deptno from emp where empno in(7566,7788));
+ 
+select mgr,deptno from emp where empno in(7566,7788);
+
+select empno,ename,deptno,sal from emp where sal >ANY(select sal from emp where empno in(7566,7788)); 
+
+select empno,ename,deptno,sal from emp where sal <ANY(select sal from emp where empno in(7566,7788));
+
+select empno,ename,deptno,sal from emp where sal <ALL(select sal from emp where empno in (7566,7788));
